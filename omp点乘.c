6ecb39c6421ff74169372fa28c3c6c0f3a9f278e
tp-tp-tp <2101324001@qq.com>
2017-12-10 20:02:42 +0800

    第三次作业
    acc点乘和omp点乘为第二题
    其余为第三题代码

---
+++ omp点乘.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include <sys/time.h>
+
+int main()
+{
+
+    int n,i;
+    double a[1000],b[1000],sum = 0;
+    printf("输入向量的维数：");
+    scanf("%d",&n);
+    printf("输入A向量：");
+    for(i = 0;i<n;i++) scanf("%lf",&a[i]);
+    printf("输入b向量：");
+    for(i = 0;i<n;i++) scanf("%lf",&b[i]);
+    struct timeval start,end;
+    gettimeofday(&start, NULL );
+    #pragma omp parallel for
+    for(i=0;i<n;i++) sum += a[i]*b[i];
+    printf("A·B = %lf\n",sum);
+    gettimeofday(&end, NULL );
+    int timeuse =1000000 * ( end.tv_sec - start.tv_sec ) + end.tv_usec - start.tv_usec;
+    printf("time=%d us\n",timeuse);
+    return 0;
+}
---
+++ omp素数.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <omp.h>
+#include <sys/time.h> // for gettimeofday()
+void main()
+{
+  struct timeval start,end;
+    gettimeofday(&start, NULL );
+ int i,j,sum=0;
+#pragma omp parallel for private(j) num_threads(8)
+    for(i=2;i<10000;i++)
+{
+    for(j=2;j<i;j++)
+{
+    if(i%j==0)break;
+}
+  if(i==j)
+{
+      sum+=1;
+}
+}
+  printf("geshu:%d\n",sum);
+gettimeofday(&end, NULL );
+long timeuse =1000000 * ( end.tv_sec - start.tv_sec ) + end.tv_usec - start.tv_usec;
+printf("time=%f s\n",timeuse/1000000.0);
+}
+
